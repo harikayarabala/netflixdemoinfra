be96b5a33e827b3117c9f9b84cb28848dbab6544
harikayarabala <harikayarabala123@gmail.com>
2025-08-14 13:51:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0e1989e836322f58b"
 instance_type = "t2.medium"
 key_name = "project.2"
-vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
+vpc_security_group_ids = ["sg-06f6de6a018b93060"]
 tags = {
 Name = var.instance_names[count.index]
 }
